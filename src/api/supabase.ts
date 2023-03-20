3f632131869b9d151ce3219ba8a98d1274c9bb05
David Sobral <david.sobral@valtech.com>
2023-03-20 17:36:03 -0300

    Added chalk and functions

--- src/api/supabase.ts
+++ src/api/supabase.ts
@@ -15,7 +15,13 @@ const supabase = createClient(env.supaUrl, env.supaSecret, options);
 // cria bucket
 export async function createFolder(projectName: string) {
   return supabase.storage.createBucket(projectName, { public: false });
-};
+}
+
+// deleta bucket
+export async function deleteFolder(projectName: string) {
+  await supabase.storage.emptyBucket(projectName);
+  return supabase.storage.deleteBucket(projectName);
+}
 
 // sobe arquivo para pasta específica dentro de bucket do projeto
 export async function uploadFile(filePath: string, fileName: string, projectName: string, contentType: string) {
@@ -26,14 +32,15 @@ export async function uploadFile(filePath: string, fileName: string, projectName
       file,
       {contentType: contentType, upsert: false},
     );
+    console.log(`${chalk.green(`Uploading ${fileName}`)}`);
 
     if (error) {
       throw new Error(error.toString());
-    };
+    }
   } catch (error) {
-    console.log('Something went wrong:', error);
-  };
-};
+    console.log(`${chalk.red('Something went wrong:', error)}`);
+  }
+}
 
 export async function updateFile(filePath: string, fileName: string, projectName: string, contentType: string) {
   const file = readFileSync(filePath)
@@ -42,4 +49,10 @@ export async function updateFile(filePath: string, fileName: string, projectName
     file,
     {contentType: contentType, upsert: false},
   );
-};
+
+  console.log(`${chalk.green(`Updating ${fileName}`)}`);
+}
+
+export async function deleteFile(fileName: string, projectName: string) {
+  await supabase.storage.from(projectName).remove([`${fileName}`]);
+}
