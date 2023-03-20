f9156782f210c0c64756c3e3b3b6379e753c354b
David Sobral <david.sobral@valtech.com>
2023-03-20 20:33:32 -0300

    Style improvements

--- src/api/fetch.ts
+++ src/api/fetch.ts
@@ -1,16 +1,20 @@
 import { readFile } from 'node:fs/promises';
 
-export async function getFile(type: 'html' | 'mjml' | 'png', path: string): Promise<string> {
+// const dirname = import.meta.url;
+
+export async function getFile(type: 'html' | 'mjml', path: string): Promise<string> {
   let string = '';
 
   if (type === 'html') {
-    string = (await readFile(path)).toString();
+    string = (await readFile(path + '/index.html')).toString();
     return string;
   }
 
-  if (type === 'mjml') {
-    return string;
-  }
-
+  string = (await readFile(path + '/index.mjml')).toString();
   return string;
+}
+
+export async function getImage(path: string, imageName: string): Promise<Buffer> {
+  let image: Buffer = await readFile(path + `/img/${imageName}`);
+  return image;
 }
