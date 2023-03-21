70bd364fc39bb8aef625e04711e62b2f14605202
David Sobral <david.sobral@valtech.com>
2023-03-21 07:20:56 -0300

    Added function to get __dirname and __filename

--- src/api/fetch.ts
+++ src/api/fetch.ts
@@ -1,6 +1,5 @@
 import { readFile } from 'node:fs/promises';
-
-// const dirname = import.meta.url;
+import path from 'path';
 
 export async function getFile(type: 'html' | 'mjml', path: string): Promise<string> {
   let string = '';
@@ -18,3 +17,5 @@ export async function getImage(path: string, imageName: string): Promise<Buffer>
   let image: Buffer = await readFile(path + `/img/${imageName}`);
   return image;
 }
+
+export const dirname = path.dirname(process.argv[1]);
