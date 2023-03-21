7ffb80e0de5e3c1d492983319ec232fd88b946f4
David Sobral <david.sobral@valtech.com>
2023-03-21 10:49:18 -0300

    export command will check if project bucket exists, handles error, prompts the user for folder path, handles error, gets images and mjml file

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -1,6 +1,7 @@
 #! /usr/bin/env node
 import chalk from 'chalk';
 import { program } from 'commander';
+import { enquire } from '../api/enquire.js';
 import { isLoggedIn } from '../lib/login.js';
 import * as supabaseAPI from '../api/supabase.js';
 import { existsSync, writeFileSync } from 'node:fs';
@@ -21,22 +22,66 @@ program
 });
 
 program
-.command('export [path]')
+.command('export <name> [path]')
 .description('Exports MJML project into host server')
-.action(async (path?: string) => {
+.action(async (name: string, path?: string) => {
   if (path) {
+    let bucket: supabaseAPI.SupabaseStorageResult;
+
+    try {
+      bucket = await supabaseAPI.folderExists(name);
+      if (bucket.error) {
+        throw new Error('BUCKET ERROR: bucket doesn\'t exist! Use \'mailer bucket -c [name]\' to create one before trying to export a project.')
+      }
+    }
+
+    catch (e) {
+      console.error(`${chalk.red(e)}`);
+      process.exit(1);
+    }
+
     const mjml = await getMJML(path);
     const images = await getImages(path);
 
-    console.log(mjml);
-    console.log(images);
+    // console.log(mjml);
+    // console.log(images);
   } else {
-    const path = await getPath();
+    let bucket: supabaseAPI.SupabaseStorageResult;
+
+    try {
+      bucket = await supabaseAPI.folderExists(name);
+      if (bucket.error) {
+        throw new Error('BUCKET ERROR: bucket doesn\'t exist! Use \'mailer bucket -c [name]\' to create one before trying to export a project.')
+      }
+    }
+
+    catch (e) {
+      console.error(`${chalk.red(e)}`);
+      process.exit(1);
+    }
+
+    try {
+      path = await getPath();
+      if (path === 'cancelled') {
+        throw new Error('Cancelled by the user');
+      }
+    }
+
+    catch (error) {
+      console.error(`${chalk.magenta(error)}`);
+      process.exit(1);
+    }
+
     const mjml = await getMJML(path);
     const images = await getImages(path);
 
-    console.log(mjml);
-    console.log(images);
+    // console.log(mjml);
+    // console.log(images);
+
+
+    // subir mjml em forma de arquivo de texto
+    // subir imagens
+    // checar se arquivos foram upados com sucesso
   }
 });
 
