63bdf2315f400033c38648411e11ec90f250d178
David Sobral <david.sobral@valtech.com>
2023-03-21 10:47:09 -0300

    Added error handling to getFile and getImage

--- src/api/fetch.ts
+++ src/api/fetch.ts
@@ -1,21 +1,47 @@
 import { readFile } from 'node:fs/promises';
+import chalk from 'chalk';
 import path from 'path';
 
 export async function getFile(type: 'html' | 'mjml', path: string): Promise<string> {
-  let string = '';
+  let string: any;
 
   if (type === 'html') {
-    string = (await readFile(path + '/index.html')).toString();
-    return string;
+    try {
+      string = await readFile(path + '/index.html');
+      return string.toString();
+    }
+
+    catch (error) {
+      console.error(`${chalk.red(error)}`);
+      process.exit(1);
+    }
   }
 
-  string = (await readFile(path + '/index.mjml')).toString();
-  return string;
+  try {
+    string = (await readFile(path + '/index.mjml')).toString();
+    return string.toString();
+  }
+
+  catch (error) {
+    console.error(`${chalk.red(error)}`);
+    process.exit(1);
+  }
 }
 
 export async function getImage(path: string, imageName: string): Promise<Buffer> {
-  let image: Buffer = await readFile(path + `/img/${imageName}`);
-  return image;
+  let image: Buffer;
+  try {
+    image = await readFile(path + `/img/${imageName}`);
+    return image;
+  }
+
+  catch (error) {
+    console.error(`${chalk.red(error)}`);
+    process.exit(1);
+  }
 }
 
 export const dirname = path.dirname(process.argv[1]);
+
+// console.log(await getFile('mjml', './test'));
+// console.log(await getImage('./test', 'header.png'));
