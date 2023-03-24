8bd2c939a13ef07c786d8c1f11ef97c19aa54b23
David Sobral <david.sobral@valtech.com>
2023-03-24 16:24:37 -0300

    The divToTable function now fully parses the HTML for top and secondary divs and ghost tables

--- src/lib/prepare.ts
+++ src/lib/prepare.ts
@@ -2,8 +2,7 @@ import chalk from 'chalk';
 import mjml2html from 'mjml';
 import __dirname from '../api/dirname.js';
 import { downloadFile } from '../api/supabase.js';
-import { MJMLParseResults } from 'mjml-core/index.js';
-import { readFileSync, writeFileSync } from 'node:fs';
+// import { readFileSync, writeFileSync } from 'node:fs';
 import beautify, { HTMLBeautifyOptions } from 'js-beautify';
 
 const { html_beautify } = beautify
@@ -39,7 +38,7 @@ export type MJMLBuffer = Buffer;
 export function parseMJML(mjml: string, marketo?: boolean) {
   const string = mjml;
   const htmlObject = mjml2html(string);
-  const html = beautifyHTML(htmlObject);
+  const html = beautifyHTML(htmlObject.html);
 
   if (marketo) {
     const parsedHTML =  divToTable(html);
@@ -49,9 +48,9 @@ export function parseMJML(mjml: string, marketo?: boolean) {
   return html;
 }
 
-function beautifyHTML(htmlObject: MJMLParseResults) {
-  let html = html_beautify(htmlObject.html, beautifyOptions);
-  return html;
+function beautifyHTML(html: string) {
+  let beautifiedHTML = html_beautify(html, beautifyOptions);
+  return beautifiedHTML;
 }
 
 export function divToTable(html: string) {
@@ -63,10 +62,27 @@ export function divToTable(html: string) {
   replacer = new RegExp(firstDivReg, 'g');
   string = string.replace(replacer, '<table class="mj-full-width-mobile" align="center"><tbody><tr><td class="mktoContainer" id="container" width="600" style="width: 600px;">');
 
-  // // end </div> to </td></tr></tbody></table>
-  const endDivReg = /(<\/div>\n)( *)(?=<\/body>)/;
+  // end </div> to </td></tr></tbody></table>
+  const endDivReg = /(<\/div>)(\n)( *)(?=<\/body>)/;
   replacer = new RegExp(endDivReg, 'g');
-  string = string.replace(replacer, '</td></tr></tbody></table>\n  ')
+  string = string.replace(replacer, '</td></tr></tbody></table>\n  ');
+
+  // middle second div + ghost table opening (closing div)
+  const middleDivGhost = /( *)(<\/div>)(\n)( *)(<!--)(.*)(\n)(.*)(600px;">\n *)/
+  replacer = new RegExp(middleDivGhost, 'g');
+  string = string.replace(replacer, '');
+
+  // second div + ghost table opening
+  const topDivGhost = /(<!--)(.*)(\n)(.*)(max-width:600px;">\n *)/;
+  replacer = new RegExp(topDivGhost, 'g');
+  string = string.replace(replacer, '');
+
+  const endDivGhost = /(<\/div>\n)( *<!.*\n)(?= *.*\n *.*\n *.*\n *.*\n *<\/body>)/;
+  replacer = new RegExp(endDivGhost, 'g');
+  string = string.replace(replacer, '');
+
+  // beautify
+  string = beautifyHTML(string);
 
   return string;
 }
