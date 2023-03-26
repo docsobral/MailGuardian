925b068c37d01583bc2518729953f382c9e813cd
David Sobral <david.sobral@valtech.com>
2023-03-26 18:47:19 -0300

    New watch function on export library will watch for changes on mjml file (and upload all files to bucket on first export)

--- src/lib/export.ts
+++ src/lib/export.ts
@@ -1,7 +1,11 @@
-import { getFile, getImage } from '../api/fetch.js';
-import { writeFile, readdir } from 'node:fs/promises';
+import chalk from 'chalk';
+import Watch from 'node-watch';
+import __dirname from '../api/dirname.js';
+import { readdir, unlink, writeFile } from 'node:fs/promises';
 // @ts-ignore
 import selectFolder from 'win-select-folder';
+import { getFile, getImage } from '../api/fetch.js';
+import { fileExists, listFiles, updateFile, uploadFile } from '../api/supabase.js';
 
 type Images = {
   [name: string]: Buffer
@@ -48,6 +52,66 @@ export async function getImages(path: string): Promise<Images> {
   return images;
 }
 
-export async function watch(path: string) {
-  
+function capitalizeFirstLetter(string: string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+export async function watch(folderPath: string, projectName: string) {
+  console.log(`${chalk.yellow(`Watching MJML`)}`);
+
+  // @ts-ignore
+  Watch(folderPath + '\\index.mjml', async (evt: string, filePath: string) => {
+    console.log(`${chalk.blue(`${capitalizeFirstLetter(evt)} detected at ${filePath}`)}`);
+
+    const mjml = await getFile('mjml', folderPath);
+    const filesInBucket = await listFiles(projectName);
+    const mjmlExists = await fileExists('index.mjml', filesInBucket.data);
+
+    if (!mjmlExists) {
+      console.log(`${chalk.blue('Sending files to bucket')}`);
+      await uploadFile(mjml, 'index.mjml', projectName);
+
+      const images = await getImages(folderPath);
+
+      Object.keys(images).forEach(async (imageName) => {
+        try {
+          const upload = await uploadFile(images[imageName], `img/${imageName}`, projectName, 'image/png');
+          if (upload.error) {
+            throw new Error(`Failed to upload ${imageName}! ${upload.error.message}`);
+          }
+          console.log(`${chalk.blue('Succesfully uploaded', imageName)}`);
+        }
+
+        catch (error) {
+          console.error(`${chalk.red(error)}`);
+        }
+      });
+    }
+
+    const files = await readdir(__dirname + 'temp');
+    for (let file of files) {
+      await unlink(__dirname + 'temp\\' + file);
+    }
+
+    try {
+      await writeFile(__dirname + 'temp\\index.mjml', mjml);
+
+      if (mjmlExists) {
+        console.log(`${chalk.blue('Updating MJML')}`);
+        try {
+          await updateFile(mjml, 'index.mjml', projectName);
+          console.log(`${chalk.blue('Success!')}`);
+        }
+
+        catch (error) {
+          console.error(`${chalk.red(error)}`);
+        }
+      }
+    }
+
+    catch (error) {
+      console.error(`${chalk.red(error)}`);
+      process.exit(1);
+    }
+  });
 }
