962a40b650893882ebf0a4f09452557b6faf222e
David Sobral <david.sobral@valtech.com>
2023-03-28 13:19:26 -0300

    0.5.11 - Removed downloads folder from package and added new configurations to tsconfig

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -12,7 +12,7 @@ import { getMJML, getImages, getPath, watch } from '../lib/export.js';
 import { enquire, PromptMessages, PromptNames, PromptTypes } from '../api/enquire.js';
 import { existsSync, mkdirSync, writeFileSync, readdirSync, unlinkSync, readFileSync } from 'node:fs';
 
-program.version('0.5.10');
+program.version('0.5.11');
 
 program
 .command('login')
