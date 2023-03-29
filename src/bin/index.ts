a55d1a701a0b2fdc997f2403685b092fd922bd3b
David Sobral <105401107+orchidb0y@users.noreply.github.com>
2023-03-29 07:43:09 -0300

    Refactor/error handling (#3)
    * Export: wrapped callback in try-catch
    
    * Login: cleaned try-chatch
    
    * Bucket: wrapped callback with try-catch
    
    * Suppresed ExperimentalWarning logs
    
    * Prepare: wrapped callback in try-catch
    
    * Mail: wrapped callback in try-catch
    
    * GNU AGPLv3
    
    * Updated README

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -1,4 +1,19 @@
 #! /usr/bin/env node
+
+const { emitWarning } = process;
+
+process.emitWarning = (warning, ...args) => {
+  if (args[0] === 'ExperimentalWarning') {
+    return;
+  }
+
+  if (args[0] && typeof args[0] === 'object' && args[0].type === 'ExperimentalWarning') {
+    return;
+  }
+  // @ts-ignore
+  return emitWarning(warning, ...args);
+}
+
 import chalk from 'chalk';
 import { program } from 'commander';
 import __dirname from '../api/dirname.js';
@@ -12,7 +27,7 @@ import { getMJML, getImages, getPath, watch } from '../lib/export.js';
 import { enquire, PromptMessages, PromptNames, PromptTypes } from '../api/enquire.js';
 import { existsSync, mkdirSync, writeFileSync, readdirSync, unlinkSync, readFileSync } from 'node:fs';
 
-program.version('0.5.11');
+program.version('0.5.14');
 
 program
 .command('login')
@@ -35,21 +50,11 @@ program
 
       if (confirm) {
         console.log(`${chalk.yellow('\nLogging in...')}`);
-
-        try {
-          const success = await login(id, password);
-
-          if (!success) {
-            throw new Error('Failed to login!');
-          }
-
-          console.log(`${chalk.blueBright('Success! Saving your credentials')}`);
+        const success = await login(id, password);
+        if (!success) {
+          throw new Error('Failed to login!');
         }
-
-        catch (error) {
-          console.error(`${chalk.red(error)}`);
-          process.exit(1);
-        }
+        console.log(`${chalk.blueBright('Success! Saving your credentials')}`);
       }
 
       else {
@@ -60,11 +65,9 @@ program
 
     else {
       const success = await login(id, password);
-
       if (!success) {
         throw new Error('Failed to login!');
       }
-
       console.log(`${chalk.blueBright('Success! Saving your credentials')}`);
     }
   }
@@ -83,99 +86,78 @@ program
 .option('-w, --watch', 'Watches template\'s folder for changes and updates bucket accordingly')
 .option('-n, --new-path', 'Ignore and overwrite current saved path')
 .action(async (name: string, path: string, options) => {
-  const paths = await getPaths();
+  try {
+    const paths = await getPaths();
 
-  for (const entry of paths) {
-    if (entry[0] === name && !options.newPath) {
-      path = entry[1];
+    for (const entry of paths) {
+      if (entry[0] === name && !options.newPath) {
+        path = entry[1];
+      }
     }
-  }
 
-  if (path) {
-    try {
+    if (path) {
       const check = existsSync(path);
       if (!check) {
         throw new Error('The path provided is broken')
       }
       savePath(name, path);
-    }
 
-    catch (error) {
-      console.error(`${chalk.red(error)}`);
-      process.exit(1);
-    }
+      let bucket: supabaseAPI.SupabaseStorageResult;
 
-    let bucket: supabaseAPI.SupabaseStorageResult;
-
-    try {
       bucket = await supabaseAPI.folderExists(name);
       if (bucket.error) {
         throw new Error('BUCKET ERROR: bucket doesn\'t exist! Use \'mailer bucket -c [name]\' to create one before trying to export a project.')
       }
-    }
 
-    catch (e) {
-      console.error(`${chalk.red(e)}`);
-      process.exit(1);
-    }
-
-    if (options.watch) {
-      await watch(path, name);
-    }
-
-    else {
-      const mjml = await getMJML(path);
-      const images = await getImages(path);
-
-      console.log(`${chalk.yellow('\nCleaning bucket before upload...')}`);
-      console.log(`${chalk.blue((await supabaseAPI.cleanFolder(name)).data?.message)}`);
-
-      try {
-        console.log(`${chalk.green('\nUploading mjml file...')}`);
-        const upload = await supabaseAPI.uploadFile(mjml, 'index.mjml', name);
-        if (upload.error) {
-          throw new Error('Failed to upload mjml file!');
-        }
-        console.log(`${chalk.blue('Upload succesfull!')}`);
+      if (options.watch) {
+        await watch(path, name);
       }
 
-      catch (error) {
-        console.error(`${chalk.red(error)}`);
-      }
+      else {
+        const mjml = await getMJML(path);
+        const images = await getImages(path);
 
-      console.log(`${chalk.green('\nUploading images...')}`);
-      Object.keys(images).forEach(async (imageName) => {
+        console.log(`${chalk.yellow('\nCleaning bucket before upload...')}`);
+        console.log(`${chalk.blue((await supabaseAPI.cleanFolder(name)).data?.message)}`);
+
         try {
-          const upload = await supabaseAPI.uploadFile(images[imageName], `img/${imageName}`, name, 'image/png');
+          console.log(`${chalk.green('\nUploading mjml file...')}`);
+          const upload = await supabaseAPI.uploadFile(mjml, 'index.mjml', name);
           if (upload.error) {
-            throw new Error(`Failed to upload ${imageName}! ${upload.error.message}`);
+            throw new Error('Failed to upload mjml file!');
           }
-          console.log(`${chalk.blue('Succesfully uploaded', imageName)}`);
+          console.log(`${chalk.blue('Upload succesfull!')}`);
         }
 
         catch (error) {
           console.error(`${chalk.red(error)}`);
         }
-      });
+
+        console.log(`${chalk.green('\nUploading images...')}`);
+        Object.keys(images).forEach(async (imageName) => {
+          try {
+            const upload = await supabaseAPI.uploadFile(images[imageName], `img/${imageName}`, name, 'image/png');
+            if (upload.error) {
+              throw new Error(`Failed to upload ${imageName}! ${upload.error.message}`);
+            }
+            console.log(`${chalk.blue('Succesfully uploaded', imageName)}`);
+          }
+
+          catch (error) {
+            console.error(`${chalk.red(error)}`);
+          }
+        });
+      }
     }
-  }
 
-  else {
-    let bucket: supabaseAPI.SupabaseStorageResult;
+    else {
+      let bucket: supabaseAPI.SupabaseStorageResult;
 
-    try {
       bucket = await supabaseAPI.folderExists(name);
       if (bucket.error) {
         throw new Error('BUCKET ERROR: bucket doesn\'t exist! Use \'mailer bucket -c [name]\' to create one before trying to export a project.')
       }
-    }
 
-    catch (e) {
-      console.error(`${chalk.red(e)}`);
-      process.exit(1);
-    }
-
-    try {
       path = await getPath();
 
       if (path === 'cancelled') {
@@ -189,52 +171,52 @@ program
       }
 
       savePath(name, path);
-    }
 
-    catch (error) {
-      console.error(`${chalk.red(error)}`);
-      process.exit(1);
-    }
-
-    if (options.watch) {
-      await watch(path, name);
-    }
-
-    else {
-      const mjml = await getMJML(path);
-      const images = await getImages(path);
-
-      console.log(`${chalk.yellow('\nCleaning bucket before upload...')}`);
-      console.log(`${chalk.blue((await supabaseAPI.cleanFolder(name)).data?.message)}`);
-
-      try {
-        console.log(`${chalk.green('\nUploading mjml file...')}`);
-        const upload = await supabaseAPI.uploadFile(mjml, 'index.mjml', name);
-        if (upload.error) {
-          throw new Error('Failed to upload mjml file!');
-        }
-        console.log(`${chalk.blue('Upload succesfull!')}`);
+      if (options.watch) {
+        await watch(path, name);
       }
 
-      catch (error) {
-        console.error(`${chalk.red(error)}`);
-      }
+      else {
+        const mjml = await getMJML(path);
+        const images = await getImages(path);
 
-      console.log(`${chalk.green('\nUploading images...')}`);
-      Object.keys(images).forEach(async (imageName) => {
+        console.log(`${chalk.yellow('\nCleaning bucket before upload...')}`);
+        console.log(`${chalk.blue((await supabaseAPI.cleanFolder(name)).data?.message)}`);
+
         try {
-          const upload = await supabaseAPI.uploadFile(images[imageName], `img/${imageName}`, name, 'image/png');
+          console.log(`${chalk.green('\nUploading mjml file...')}`);
+          const upload = await supabaseAPI.uploadFile(mjml, 'index.mjml', name);
           if (upload.error) {
-            throw new Error(`Failed to upload ${imageName}! ${upload.error.message}`);
+            throw new Error('Failed to upload mjml file!');
           }
-          console.log(`${chalk.blue(`Succesfully uploaded ${imageName}`)}`);
+          console.log(`${chalk.blue('Upload succesfull!')}`);
         }
 
         catch (error) {
           console.error(`${chalk.red(error)}`);
         }
-      });
+
+        console.log(`${chalk.green('\nUploading images...')}`);
+        Object.keys(images).forEach(async (imageName) => {
+          try {
+            const upload = await supabaseAPI.uploadFile(images[imageName], `img/${imageName}`, name, 'image/png');
+            if (upload.error) {
+              throw new Error(`Failed to upload ${imageName}! ${upload.error.message}`);
+            }
+            console.log(`${chalk.blue(`Succesfully uploaded ${imageName}`)}`);
+          }
+
+          catch (error) {
+            console.error(`${chalk.red(error)}`);
+          }
+        });
+      }
     }
+  }
+
+  catch (error) {
+    console.error(`${chalk.red(error)}`);
+    process.exit(1);
   }
 });
 
@@ -246,23 +228,29 @@ program
 .option('-c, --create', 'creates a bucket')
 .option('-l, --list', 'lists all buckets')
 .action(async (name, options) => {
-  if (options.create) {
-    console.log(`${chalk.yellow(`Creating bucket named ${name}`)}`);
-    supabaseAPI.createFolder(name);
-    return
-  }
+  try {
+    if (options.create) {
+      console.log(`${chalk.yellow(`Creating bucket named ${name}`)}`);
+      const { data, error } = await supabaseAPI.createFolder(name);
+      if (error) {
+        throw new Error(`${error.stack?.slice(17)}`);
+      }
+      return;
+    }
 
-  if (options.delete) {
-    console.log(`${chalk.magenta(`Deleting bucket named ${name}`)}`);
-    supabaseAPI.deleteFolder(name);
-    return
-  }
+    if (options.delete) {
+      console.log(`${chalk.magenta(`Deleting bucket named ${name}`)}`);
+      const { data, error } = await supabaseAPI.deleteFolder(name);
+      if (error) {
+        throw new Error(`${error.stack?.slice(17)}`);
+      }
+      return;
+    }
 
-  if (options.list) {
-    try {
+    if (options.list) {
       const { data, error } = await supabaseAPI.listBuckets();
       if (error) {
-        throw new Error(error.message);
+        throw new Error(`${error.stack?.slice(17)}`);
       }
 
       if (data) {
@@ -272,11 +260,11 @@ program
         }
       }
     }
+  }
 
-    catch (error) {
-      console.error(`${chalk.red(error)}`);
-      process.exit(1);
-    }
+  catch (error) {
+    console.log(`${chalk.red(error)}`);
+    process.exit(1);
   }
 });
 
@@ -286,89 +274,69 @@ program
 .argument('<name>', 'Name of the bucket where the MJML you want to parse is located')
 .option('-m, --marketo', 'parses MJML for Marketo', false)
 .action(async (name, options) => {
-  // check is bucket exists
   try {
+    // check is bucket exists
     const bucket = await supabaseAPI.folderExists(name);
     if (bucket.error) {
       throw new Error('BUCKET ERROR: bucket doesn\'t exist! Use \'mailer bucket -c [name]\' to create one before trying to export a project.')
     }
-  }
 
-  catch (e) {
-    console.error(`${chalk.red(e)}`);
-    process.exit(1);
-  }
-
-  // check if temp folder exists
-  if (!existsSync(__dirname + 'temp')) {
-    mkdirSync(__dirname + 'temp');
-  }
-
-  else {
-    const files = readdirSync(__dirname + 'temp');
-    for (let file of files) {
-      unlinkSync(__dirname + 'temp\\' + file);
-    }
-  }
-
-  // fetches mjml file
-  console.log(`${chalk.yellow('Fetching index.mjml file from the', name, 'bucket')}`);
-  const mjmlBlob = await downloadMJML(name);
-  if (mjmlBlob) {
-    let mjmlString = await mjmlBlob.text()
-    let imgList: string[] = [];
-    let signedUrlList: string[] = [];
-
-    // get list of images
-    try {
-      const fetch = await supabaseAPI.listImages(name);
-      if (fetch.error) {
-        throw new Error('Failed to fetch list of image names!');
-      }
-
-      fetch.data.forEach(fileObject => imgList.push(fileObject.name));
-    }
-
-    catch (error) {
-      console.error(`${chalk.red(error)}`);
-      process.exit(1);
-    }
-
-    // get list of signes urls
-    try {
-      const fetch = await supabaseAPI.imagesUrls(name, imgList);
-      if (fetch.error) {
-        throw new Error('Failed to get signed URLs!');
-      }
-
-      fetch.data.forEach(object => signedUrlList.push(object.signedUrl));
-    }
-
-    catch (error) {
-      console.error(`${chalk.red(error)}`);
-    }
-
-    // replace local paths for remote paths
-    for (let index in imgList) {
-      const localPath = `(?<=src=")(.*)(${imgList[index]})(?=")`;
-      const replacer = new RegExp(localPath, 'g');
-      mjmlString = mjmlString.replace(replacer, signedUrlList[index]);
-    };
-
-    // save mjml with new paths
-    writeFileSync(__dirname + 'temp\\index.mjml', mjmlString);
-
-    if (options.marketo) {
-      const parsedHTML = parseMJML(readFileSync(__dirname + 'temp\\index.mjml', { encoding: 'utf8' }), true);
-      writeFileSync(__dirname + `temp\\parsed.html`, parsedHTML);
+    // check if temp folder exists
+    if (!existsSync(__dirname + 'temp')) {
+      mkdirSync(__dirname + 'temp');
     }
 
     else {
-      const parsedHTML = parseMJML(readFileSync(__dirname + 'temp\\index.mjml', { encoding: 'utf8' }));
-      writeFileSync(__dirname + 'temp\\parsed.html', parsedHTML);
+      const files = readdirSync(__dirname + 'temp');
+      for (let file of files) {
+        unlinkSync(__dirname + 'temp\\' + file);
+      }
     }
 
-    try {
+    // fetches mjml file
+    console.log(`${chalk.yellow('Fetching index.mjml file from the', name, 'bucket')}`);
+    const mjmlBlob = await downloadMJML(name);
+    if (mjmlBlob) {
+      let mjmlString = await mjmlBlob.text()
+      let imgList: string[] = [];
+      let signedUrlList: string[] = [];
+
+      // get list of images
+      const firstFetch = await supabaseAPI.listImages(name);
+      if (firstFetch.error) {
+        throw new Error('Failed to fetch list of image names!');
+      }
+
+      firstFetch.data.forEach(fileObject => imgList.push(fileObject.name));
+
+      // get list of signes urls
+      const secondFetch = await supabaseAPI.imagesUrls(name, imgList);
+      if (secondFetch.error) {
+        throw new Error('Failed to get signed URLs!');
+      }
+
+      secondFetch.data.forEach(object => signedUrlList.push(object.signedUrl));
+
+      // replace local paths for remote paths
+      for (let index in imgList) {
+        const localPath = `(?<=src=")(.*)(${imgList[index]})(?=")`;
+        const replacer = new RegExp(localPath, 'g');
+        mjmlString = mjmlString.replace(replacer, signedUrlList[index]);
+      };
+
+      // save mjml with new paths
+      writeFileSync(__dirname + 'temp\\index.mjml', mjmlString);
+
+      if (options.marketo) {
+        const parsedHTML = parseMJML(readFileSync(__dirname + 'temp\\index.mjml', { encoding: 'utf8' }), true);
+        writeFileSync(__dirname + `temp\\parsed.html`, parsedHTML);
+      }
+
+      else {
+        const parsedHTML = parseMJML(readFileSync(__dirname + 'temp\\index.mjml', { encoding: 'utf8' }));
+        writeFileSync(__dirname + 'temp\\parsed.html', parsedHTML);
+      }
+
       const list = await supabaseAPI.listFiles(name);
       const exists = await supabaseAPI.fileExists(`${options.marketo? 'marketo.html' : 'index.html'}`, list.data);
 
@@ -382,11 +350,11 @@ program
       }
       console.log(`${chalk.blue('Successfully parsed MJML and uploaded HTML to server')}`);
     }
+  }
 
-    catch (error) {
-      console.error(`${chalk.red(error)}`);
-      process.exit(1);
-    }
+  catch (error) {
+    console.error(`${chalk.red(error)}`);
+    process.exit(1);
   }
 });
 
@@ -397,45 +365,39 @@ program
 .argument('<recipients>', 'Recipient list (e.g. "davidsobral@me.com, davidcsobral@gmail.com"')
 .option('-m, --marketo', 'sends the Marketo compatible HTML')
 .action(async (name: string, recipientsString: string, options) => {
-  const check = await isLoggedIn();
-
   try {
+    const check = await isLoggedIn();
+
     const bucket = await supabaseAPI.folderExists(name);
     if (bucket.error) {
       throw new Error('BUCKET ERROR: bucket doesn\'t exist! Use \'mailer bucket -c [name]\' to create one before trying to export a project.')
     }
-  }
 
-  catch (e) {
-    console.error(`${chalk.red(e)}`);
-    process.exit(1);
-  }
-
-  if (typeof check === 'string') {
-    process.exit(1);
-  }
-
-  if (typeof check === 'boolean') {
-    if (!check) {
-      console.error(`${chalk.red('Please log in with "mailer login" before trying to send an email')}`);
+    if (typeof check === 'string') {
       process.exit(1);
     }
-  }
 
-  const recipientsList: string[] = recipientsString.split(', ')
-  const htmlBlob = await downloadHTML(name, options.marketo);
+    if (typeof check === 'boolean') {
+      if (!check) {
+        console.error(`${chalk.red('Please log in with "mailer login" before trying to send an email')}`);
+        process.exit(1);
+      }
+    }
 
-  if (htmlBlob) {
-    const htmlString = await htmlBlob.text();
-    console.log(`${chalk.yellow('Sending email...')}`);
-    try {
+    const recipientsList: string[] = recipientsString.split(', ')
+    const htmlBlob = await downloadHTML(name, options.marketo);
+
+    if (htmlBlob) {
+      const htmlString = await htmlBlob.text();
+      console.log(`${chalk.yellow('Sending email...')}`);
       await mailHTML(recipientsList, htmlString);
       console.log(`${chalk.blue('Success!')}`);
     }
+  }
 
-    catch (error) {
-      console.error(`${chalk.red(error)}`);
-    }
+  catch (error) {
+    console.error(`${chalk.red(error)}`);
+    process.exit(1);
   }
 });
 
