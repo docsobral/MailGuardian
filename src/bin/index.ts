4213c667eb44378561f85d99ec19ad258def9cbc
David Sobral <david.sobral@valtech.com>
2023-04-01 15:55:06 -0300

    0.6.6

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -27,7 +27,7 @@ import { getMJML, getImages, getPath, watch } from '../lib/export.js';
 import { enquire, PromptMessages, PromptNames, PromptTypes } from '../api/enquire.js';
 import { existsSync, mkdirSync, writeFileSync, readdirSync, unlinkSync, readFileSync } from 'node:fs';
 
-program.version('0.6.5');
+program.version('0.6.6');
 
 program
 .command('login')
