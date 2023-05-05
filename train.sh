2da9a7813dd06814b0ffe56322d32fd3ead45d7e
David Sobral <david.sobral@valtech.com>
2023-05-05 09:03:37 -0300

    v.0.12.12 - added eslint

--- train.sh
+++ train.sh
@@ -8,16 +8,14 @@ docker cp node_modules/@stdlib/datasets-spam-assassin/data/spam-1 spamassassin-a
 docker cp node_modules/@stdlib/datasets-spam-assassin/data/spam-2 spamassassin-app:data/spam-2
 printf '\n%s' '  Done!'
 
-# # Run training
+# Run training
 printf '\n\n%s\n  ' '  Training ham...'
 docker exec spamassassin-app sa-learn --ham data/easy-ham-1 data/easy-ham-2 data/hard-ham-1
-printf '%s' '  Done!'
 
 printf '\n\n%s\n  ' '  Training spam...'
 docker exec spamassassin-app sa-learn --spam data/spam-1 data/spam-2
-printf '%s' '  Done!'
 
-# # Delete training files
+# Delete training files
 printf '\n\n%s' '  Deleting training files...'
 docker exec --user root spamassassin-app rm -rf data/easy-ham-1 data/easy-ham-2 data/hard-ham-1 data/spam-1 data/spam-2
 printf '\n%s' '  Done!'
