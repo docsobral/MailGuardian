ccdf06a6eff08dc8568d055be494a92ef1c29ce9
David Sobral <david.sobral@valtech.com>
2023-08-02 11:39:37 -0300

    Change base styles

--- src/api/filesystem.ts
+++ src/api/filesystem.ts
@@ -125,9 +125,7 @@ export async function createFolders(templateName: string): Promise<void> {
 const newMJML = `<mjml>
   <mj-head>
     <mj-style>
-      @media(max-width: 480px) {
-
-      }
+       
     </mj-style>
   </mj-head>
 
