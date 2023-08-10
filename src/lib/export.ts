00e331b6076007e0162f855cc519ac47a9aee328
David Sobral <david.sobral@valtech.com>
2023-08-10 08:17:14 -0300

    Switch to broadcaster

--- src/lib/export.ts
+++ src/lib/export.ts
@@ -1,11 +1,16 @@
-import ora from 'ora';
-import chalk from 'chalk';
-import Watch from 'node-watch';
+// import chalk from 'chalk';
+// import Watch from 'node-watch';
 import { readdir } from 'node:fs/promises';
 import selectFolder from 'win-select-folder';
+import { broadcaster } from '../bin/index.js';
 import { __dirname } from '../api/filesystem.js';
 import { getFile, getImage } from '../api/filesystem.js';
-import { listFiles, fileExists, uploadFile, updateFile } from '../api/supabase.js';
+import {
+  // listFiles,
+  // fileExists,
+  uploadFile,
+  // updateFile
+} from '../api/supabase.js';
 
 type Images = {
   [name: string]: Buffer
@@ -52,106 +57,105 @@ export async function getImages(path: string): Promise<Images> {
   return images;
 }
 
-function capitalizeFirstLetter(string: string): string {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
+// function capitalizeFirstLetter(string: string): string {
+//   return string.charAt(0).toUpperCase() + string.slice(1);
+// }
 
-export async function watch(folderPath: string, projectName: string, marketo: boolean = false): Promise<void> {
-  const mjml = await getFile('mjml', folderPath, marketo);
-  const filesInBucket = await listFiles(projectName);
-  const fileName = marketo ? 'marketo.mjml' : 'index.mjml';
-  const mjmlExists = await fileExists(fileName, filesInBucket.data);
-  const type = marketo ? 'marketo' : 'index';
+// export async function watch(folderPath: string, projectName: string, marketo: boolean = false): Promise<void> {
+//   const mjml = await getFile('mjml', folderPath, marketo);
+//   const filesInBucket = await listFiles(projectName);
+//   const fileName = marketo ? 'marketo.mjml' : 'index.mjml';
+//   const mjmlExists = await fileExists(fileName, filesInBucket.data);
+//   const type = marketo ? 'marketo' : 'index';
 
-  if (!mjmlExists) {
-    try {
-      console.log(`${chalk.blue('Sending files to bucket')}`);
-      const upload = await uploadFile(mjml, `${type}.mjml`, projectName);
-      console.log(`${chalk.blue(`Successfully uploaded ${type}.mjml`)}`);
-      if (upload.error) {
-        throw new Error(`Failed to upload MJML!! ${upload.error.message}`);
-      }
-    }
+//   if (!mjmlExists) {
+//     try {
+//       console.log(`${chalk.blue('Sending files to bucket')}`);
+//       const upload = await uploadFile(mjml, `${type}.mjml`, projectName);
+//       console.log(`${chalk.blue(`Successfully uploaded ${type}.mjml`)}`);
+//       if (upload.error) {
+//         throw new Error(`Failed to upload MJML!! ${upload.error.message}`);
+//       }
+//     }
 
-    catch (error) {
-      console.error(`${chalk.red(error)}`);
-    }
+//     catch (error) {
+//       console.error(`${chalk.red(error)}`);
+//     }
 
-    const images = await getImages(folderPath);
+//     const images = await getImages(folderPath);
 
-    Object.keys(images).forEach(async (imageName) => {
-      try {
-        const upload = await uploadFile(images[imageName], `img/${imageName}`, projectName, 'image/png');
-        if (upload.error) {
-          throw new Error(`Failed to upload ${imageName}! ${upload.error.message}`);
-        }
-        console.log(`${chalk.blue('Succesfully uploaded', imageName)}`);
-      }
+//     Object.keys(images).forEach(async (imageName) => {
+//       try {
+//         const upload = await uploadFile(images[imageName], `img/${imageName}`, projectName, 'image/png');
+//         if (upload.error) {
+//           throw new Error(`Failed to upload ${imageName}! ${upload.error.message}`);
+//         }
+//         console.log(`${chalk.blue('Succesfully uploaded', imageName)}`);
+//       }
 
-      catch (error) {
-        console.error(`${chalk.red(error)}`);
-      }
-    });
-  }
+//       catch (error) {
+//         console.error(`${chalk.red(error)}`);
+//       }
+//     });
+//   }
 
-  console.log(`${chalk.yellow(`Watching MJML for changes\n`)}`);
+//   console.log(`${chalk.yellow(`Watching MJML for changes\n`)}`);
 
-  // @ts-ignore
-  Watch(folderPath + `\\${type}.mjml`, async (evt: string, filePath: string) => {
-    console.log(`${chalk.yellow(`${capitalizeFirstLetter(evt)} detected at ${filePath}`)}`);
-    const newMJML = await getFile('mjml', folderPath, marketo);
+//   // @ts-ignore
+//   Watch(folderPath + `\\${type}.mjml`, async (evt: string, filePath: string) => {
+//     console.log(`${chalk.yellow(`${capitalizeFirstLetter(evt)} detected at ${filePath}`)}`);
+//     const newMJML = await getFile('mjml', folderPath, marketo);
 
-    try {
-      console.log(`${chalk.blue('Updating MJML')}`);
-      await updateFile(newMJML, `${type}.mjml`, projectName);
-      console.log(`${chalk.blue('Success!\n')}`);
-    }
+//     try {
+//       console.log(`${chalk.blue('Updating MJML')}`);
+//       await updateFile(newMJML, `${type}.mjml`, projectName);
+//       console.log(`${chalk.blue('Success!\n')}`);
+//     }
 
-    catch (error) {
-      console.error(`${chalk.red(error)}`);
-      process.exit(1);
-    }
-  });
-}
+//     catch (error) {
+//       console.error(`${chalk.red(error)}`);
+//       process.exit(1);
+//     }
+//   });
+// }
 
 export async function uploadMJML(bucketName: string, path: string, marketo: boolean = false): Promise<void> {
   try {
     const fileName = marketo ? 'marketo.mjml' : 'index.mjml';
 
-    process.stdout.write('\n');
-    const spinner = ora(`${chalk.yellow(`Uploading ${fileName} file...`)}`).start();
-    const mjml = await getMJML(path, marketo);
+    broadcaster.start(`Uploading ${fileName} file...`);
 
+    const mjml = await getMJML(path, marketo);
     const upload = await uploadFile(mjml, `${fileName}`, bucketName, 'text/plain');
+
     if (upload.error) {
-      spinner.fail(`${chalk.red(`Failed to upload ${fileName} file!\n`)}`);
+      broadcaster.fail(`Failed to upload ${fileName} file!\n`)
       throw new Error(upload.error.stack);
     }
 
-    spinner.succeed(`${chalk.yellow(`Successfully uploaded ${fileName} file!`)}`);
+    broadcaster.succeed(`Successfully uploaded ${fileName} file!`);
   }
 
   catch (error) {
-    console.warn(`${chalk.red(error)}`);
+    broadcaster.warn(error as string);
   }
 }
 
 export async function uploadImages(bucketName: string, path: string): Promise<void> {
   const images = await getImages(path);
 
-  process.stdout.write('\n');
-  const spinner = ora(`${chalk.yellow('Uploading images...')}`).start();
+  broadcaster.start('Uploading images...')
 
   Object.keys(images).forEach(async (imageName) => {
     const upload = await uploadFile(images[imageName], `img/${imageName}`, bucketName, 'image/png');
     if (upload.error) {
-      spinner.text = `${spinner.text}\n${chalk.red(`  Failed to upload ${imageName}! ${upload.error.message}`)}`;
+      broadcaster.append(`  Failed to upload ${imageName}! ${upload.error.message}`, 'red', true);
     }
-    spinner.text = `${spinner.text}\n${chalk.blue('  Succesfully uploaded', imageName)}`;
+    broadcaster.append(`  Succesfully uploaded ${imageName}`, 'blue', true);
   });
 
   await delay(1000);
-  spinner.succeed();
+  broadcaster.succeed();
 }
 
 async function delay(ms: number): Promise<unknown> {
