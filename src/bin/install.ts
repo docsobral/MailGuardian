887cf782a3dd771b8f266be3483c213a254677a0
David Sobral <david.sobral@valtech.com>
2023-08-14 21:07:24 -0300

    Improve install.ts

--- src/bin/install.ts
+++ src/bin/install.ts
@@ -1,65 +1,68 @@
 #! /usr/bin/env node
 
+import ora from "ora";
 import chalk from "chalk";
 import { resolve } from 'path';
+import { writeFile } from 'node:fs/promises';
 import { mkdirSync,  existsSync } from 'node:fs';
-import { writeFile, readFile } from 'node:fs/promises';
 import { AppState, __dirname } from "../api/filesystem.js";
 import { enquire, EnquireMessages, EnquireNames, EnquireTypes } from "../api/enquire.js";
 
-console.log('\nInstalling MailGuardian...');
+const spinner = ora();
+spinner.start(`${chalk.yellow('Installing MailGuardian...')}`);
 
-if (!existsSync(resolve(__dirname, 'config'))) {
-  console.log(`${chalk.blue('Creating save files...\n')}`);
-  mkdirSync(__dirname + 'config');
+try {
+  if (!existsSync(resolve(__dirname, 'config'))) {
+    console.log(`${chalk.blue('Creating save files...\n')}`);
+    mkdirSync(__dirname + 'config');
 
-  await writeFile(__dirname + 'config\\paths.json', JSON.stringify({}, null, 2));
+    await writeFile(__dirname + 'config\\paths.json', JSON.stringify({}, null, 2));
 
-  const initialState: AppState = {logged: [false, false]};
+    const initialState: AppState = {logged: [false, false]};
 
-  await writeFile(__dirname + 'config\\state.json', JSON.stringify(initialState, null, 2));
+    await writeFile(__dirname + 'config\\state.json', JSON.stringify(initialState, null, 2));
 
-  const answers = await enquire([
-    {
-      type: EnquireTypes.input,
-      name: EnquireNames.supabaseSecret,
-      message: EnquireMessages.supabaseSecret
-    },
+    const answers = await enquire([
+      {
+        type: EnquireTypes.input,
+        name: EnquireNames.supabaseSecret,
+        message: EnquireMessages.supabaseSecret
+      },
 
-    {
-      type: EnquireTypes.input,
-      name: EnquireNames.supabaseURL,
-      message: EnquireMessages.supabaseURL
-    },
+      {
+        type: EnquireTypes.input,
+        name: EnquireNames.supabaseURL,
+        message: EnquireMessages.supabaseURL
+      },
 
-    {
-      type: EnquireTypes.input,
-      name: EnquireNames.secretKey,
-      message: EnquireMessages.secretKey
+      {
+        type: EnquireTypes.input,
+        name: EnquireNames.secretKey,
+        message: EnquireMessages.secretKey
+      }
+    ]);
+
+    const appConfigs = {
+      'SUPA_SECRET': answers.supabaseSecret,
+      'SUPA_URL': answers.supabaseURL,
+      'SECRET_KEY': answers.secretKey,
     }
-  ]);
 
-  const appConfigs = {
-    'SUPA_SECRET': answers.supabaseSecret,
-    'SUPA_URL': answers.supabaseURL,
-    'SECRET_KEY': answers.secretKey,
-  }
-
-  await writeFile(__dirname + 'config\\config.json', JSON.stringify(appConfigs, null, 2));
-  console.log(`${chalk.yellow('Finished creating config files and terminating process. Now run \'mailer login <email> <passoword>\'.')}`);
-  process.exit(0);
-};
-
-if (!existsSync(resolve(__dirname, 'node_modules/win-select-folder/dist/select-folder.d.ts'))) {
-  const thing = 'export = selectFolder;\n\ntype FolderSelectOptions = {\n  root: string;\n  description: string;\n  newFolder: number;\n}\n\ndeclare function selectFolder(e: FolderSelectOptions, o?: any): Promise<string>;';
-
-  await writeFile(resolve(__dirname, 'node_modules/win-select-folder/dist/select-folder.d.ts'), thing, { encoding: 'utf8' });
+    await writeFile(__dirname + 'config\\config.json', JSON.stringify(appConfigs, null, 2));
+  };
 }
 
-let TSCONFIG = JSON.parse(await readFile(resolve(__dirname, 'tsconfig.json'), { encoding: 'utf8' }));
+catch (error) {
+  spinner.fail(error as string);
+}
 
-TSCONFIG.compilerOptions.noEmitOnError = true;
+async function delay(ms: number): Promise<void> {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-TSCONFIG = JSON.stringify(TSCONFIG, null, 2);
+await delay(2000);
+spinner.suffixText = chalk.bgWhite.black(`\n\n${chalk.bgBlack('  ')}Now run \'${chalk.red('mg login <email> <passoword>')}\'.`);
+spinner.suffixText += `\n\n${chalk.bgBlack('  ')}${chalk.bgRed.black.bold.underline('NOTICE: Only works with Gmail, and you must use an app password. To generate an app password, go to Manage you Google Account > Security > 2FA > App Passwords')}`;
+spinner.succeed(`${chalk.yellow('Finished installing MailGuardian!')}`);
 
-await writeFile(resolve(__dirname, 'tsconfig.json'), TSCONFIG);
+await delay(3000);
