7f2732e68a6a85b5a460d0ec7aaa4fe0f556686b
David Sobral <david.sobral@valtech.com>
2023-10-10 10:03:28 -0300

    replace-src commands replaces src urls on .html file

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -361,6 +361,64 @@ program
 });
 
 program
+.command('replace-src')
+.description('Replaces img tags\' src attributes with a remote url')
+.argument('[path]', 'Path to the folder where the HTML file is located', resolve())
+.option('-b, --bucket <bucket>', 'Specify the name of the bucket where the img files are located')
+.option('-n, --name <name>', 'Specify the name of the HTML file (e.g.: \'filename.html\')', 'index.html')
+.action(async (path: string, options: {bucket: string | undefined, name: string}) => {
+  if (!options.bucket) {
+    broadcaster.error('You must specify the bucket\'s name');
+    process.exit(1);
+  }
+
+  const supabaseAPI = await import('../api/supabase.js');
+
+  let imgList: string[] = [];
+  let signedUrlList: string[] = [];
+
+  const imageList = await supabaseAPI.listImages(options.bucket);
+
+  if (imageList.error) {
+    broadcaster.fail();
+    broadcaster.error(`Failed to fetch list of image names! ${imageList.error.stack?.slice(17)}`);
+  }
+
+  // @ts-ignore
+  imageList.data.forEach(fileObject => imgList.push(fileObject.name));
+  const signedList = await supabaseAPI.imagesUrls(options.bucket, imgList);
+
+  if (signedList.error) {
+    broadcaster.fail();
+    broadcaster.error(`Failed to get signed URLs! ${signedList.error.stack?.slice(17)}`);
+  }
+
+  // @ts-ignore
+  signedList.data.forEach(object => signedUrlList.push(object.signedUrl));
+
+  let htmlString: string = '';
+
+  try {
+    htmlString = readFileSync(resolve(path, options.name), { encoding: 'utf8' });
+  }
+
+  catch (error) {
+    broadcaster.fail();
+    broadcaster.error(`Failed to get the HTML file!\n${error}`);
+  }
+
+  // MOVE THIS FUNCTION TO A SEPARATE FILE
+  // replace local paths for remote paths
+  for (let index in imgList) {
+    const localPath = `(?<=src=")(.*)(${imgList[index]})(?=")`;
+    const replacer = new RegExp(localPath, 'g');
+    htmlString = htmlString.replace(replacer, signedUrlList[index]);
+  };
+
+  writeFileSync(resolve(path, options.name), htmlString, { encoding: 'utf8' });
+})
+
+program
 .command('build')
 .description('Compiles MJML into HTML')
 .argument('[path]', 'Path to the folder where the MJML file is located', resolve())
